59d7d2ffa7f6c8ea10a3e1f522ec7d65592bb9e6
thotluna <eladi.feijoo@gmail.com>
2024-06-01 19:13:06 -0400

    fix restart game error

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -7,22 +7,28 @@ import utils.YesNotQuestion;
 public class ConsoleView extends View {
     private final GreetingView greetingView;
     private final YesNotQuestion yesNotQuestionView;
-    private final GameView gameView;
+    private GameView gameView;
     private final GoodbyeView goodbyeView;
 
 
     public ConsoleView() {
         super();
-        Game game = new Game();
+
         greetingView = new GreetingView();
         yesNotQuestionView = new YesNotQuestion();
         goodbyeView = new GoodbyeView();
+
+    }
+
+    private void initGame(){
+        Game game = new Game();
         gameView = new GameView(game);
     }
 
     @Override
     public void interact() {
         do {
+            initGame();
             greetingView.interact();
             gameView.interact();
         }while(yesNotQuestionView.read(Message.RESUME.value()).isAffirmative());
